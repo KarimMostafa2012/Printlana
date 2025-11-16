4a854f65348a310724ad15d5072ffdf1da71b6ba
Karim Mostafa <kimomagic2012@gmail.com>
2025-11-16 12:12:35 +0200

    register form

--- wp-content/plugins/woocommerce/templates/myaccount/form-login.php
+++ wp-content/plugins/woocommerce/templates/myaccount/form-login.php
@@ -100,7 +100,6 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 
 			<?php endif; ?>
 
-			<?php do_action( 'woocommerce_register_form' ); ?>
 
 			<p class="woocommerce-form-row form-row">
 				<?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
