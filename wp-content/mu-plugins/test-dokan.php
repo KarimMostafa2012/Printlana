07d57ab38765f63a15f764a8d98a99c8a2189650
Karim Mostafa <kimomagic2012@gmail.com>
2025-12-10 11:04:39 +0200

    test dokan

--- wp-content/mu-plugins/test-dokan.php
+++ wp-content/mu-plugins/test-dokan.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * Plugin Name: test_dokan.php
+ * Description: This mu plugin tests Dokan withdraw REST API logging.
+ *  Version: 1.0.0
+ */
 add_action( 'rest_api_init', function() {
     add_filter( 'rest_pre_echo_response', function( $result, $server, $request ) {
         $route = $request->get_route();
